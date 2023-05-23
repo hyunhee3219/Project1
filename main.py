a3970c6491220355673d0090e17a245079402beb
Kim Hyun Hee <hhkim0123@naver.com>
2023-05-23 17:29:18 +0900

    복원

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ def add_func(n1, n2):
 def sub_func(n1, n2):
     return n1 - n2
 
+def mul_func(n1, n2):
+    return n1 * n2
+
+def div_func(n1, n2):
+    return n1 / n2
+
 
 
 ## 전역 변수부(+인스턴스 변수)
@@ -18,3 +24,8 @@ print(num1, '+', num2, '=', res)
 res = sub_func(num1, num2)
 print(num1, "-", num2, "=", res)
 
+res = mul_func(num1, num2)
+print(num1, "*", num2, "=", res)
+
+res = div_func(num1, num2)
+print(num1, "/", num2, "=", res)
