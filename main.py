d474a9ac6eba422ab2ef72d3828fe4d8b2c30819
Kim Hyun Hee <hhkim0123@naver.com>
2023-05-24 15:36:20 +0900

    나누기 추가

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ def mul_func(n1, n2):
 def div_func(n1, n2):
     return n1 / n2
 
+def je_func(n1, n2):
+    return n1 ** n2
 
 
 ## 전역 변수부(+인스턴스 변수)
@@ -29,3 +31,6 @@ print(num1, "*", num2, "=", res)
 
 res = div_func(num1, num2)
 print(num1, "/", num2, "=", res)
+
+res =je_func(num1, num2)
+print(num1, "**", num2, "=", res)
